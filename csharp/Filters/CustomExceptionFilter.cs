1d3d401d2bb672cc39fb6202627ae4eb265db3b5
daniel pomalaza <danichalay@yahoo.es>
2013-09-13 09:52:55 -0500

    Custom Exception Filter

---
+++ csharp/Filters/CustomExceptionFilter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace Web.Filters
+{
+    public class CustomExceptionFilter : FilterAttribute, IExceptionFilter
+    {
+        public void OnException(ExceptionContext filterContext)
+        {          
+
+                if (filterContext.ExceptionHandled) return;
+
+                //TODO: Grabar en Log
+                ManejadorLog manejadorLog = new ManejadorLog();
+
+                manejadorLog.GrabarLog(filterContext.Exception.Message + " - " + filterContext.Exception.StackTrace);
+                filterContext.Result = new RedirectToRouteResult(new RouteValueDictionary(new { controller = "Error", action = "Error" }));
+                filterContext.ExceptionHandled = true;
+                filterContext.HttpContext.Response.Clear();
+                filterContext.HttpContext.Response.StatusCode = 500;
+                filterContext.HttpContext.Response.TrySkipIisCustomErrors = true;
+            
+        }
+    }
+}
